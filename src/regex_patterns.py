b5050f6d9e5238c590dd39d3f5d5526b625f73c2
Harry Yeh <eechih@gmail.com>
2022-10-03 12:04:38 +0800

    Added strong password regex pattern

--- src/regex_patterns.py
+++ src/regex_patterns.py
@@ -2,3 +2,7 @@ import re
 
 email_pattern = r"([A-Za-z0-9]+[-+_.])*[A-Za-z0-9]+@[A-Za-z0-9]+(.[A-Z|a-z]{2,})*"
 EMAIL_PATTERN = re.compile(pattern=email_pattern)
+
+# 高強度密碼：6位數以上，並且至少包含：大寫字母、小寫字母、數字、符號各一（只允許八種符號：'@', '$', '!', '#', '%', '*', '?', '&'）
+strong_password_pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,}$"
+STRONG_PASSWORD_PATTERN = re.compile(pattern=strong_password_pattern)
--- tests/test_regex_patterns.py
+++ tests/test_regex_patterns.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.regex_patterns import EMAIL_PATTERN
+from src.regex_patterns import EMAIL_PATTERN, STRONG_PASSWORD_PATTERN
 
 
 @pytest.mark.parametrize("email,is_valid", [
@@ -23,6 +23,26 @@ from src.regex_patterns import EMAIL_PATTERN
 ])
 def test_email_regex(email, is_valid):
     mo = EMAIL_PATTERN.fullmatch(email)
+    if is_valid:
+        assert mo is not None
+    else:
+        assert mo is None
+
+
+@pytest.mark.parametrize("password,is_valid", [
+    # ---- Valid Password ----
+    ("Geek1@", True),
+    ("Geek1@Geek1@", True),
+    # ---- Invalid Password ----
+    ("Gek1@", False),  # 不足6個字元
+    ("geek1@", False),  # 缺少大寫字母
+    ("GEEK1@", False),  # 缺少小寫字母
+    ("Geek@@", False),  # 缺少數字
+    ("Geek11", False),  # 缺少符號
+    ("Geek1@_", False),  # 不接受底線符號
+])
+def test_strong_password_regex(password, is_valid):
+    mo = STRONG_PASSWORD_PATTERN.fullmatch(password)
     if is_valid:
         assert mo is not None
     else:
